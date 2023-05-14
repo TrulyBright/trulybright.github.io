79ac753bc409710a8bde360032e0d7fc14c40d20
TrulyBright <trulybright@yonsei.ac.kr>
2023-05-15 03:53:22 +0900

    serve.sh now serves at 0.0.0.0:4000

--- serve.sh
+++ serve.sh
@@ -1,4 +1,4 @@
 yarn install
 yarn build
 bundle install
-JEKYLL_ENV=production bundle exec jekyll serve
+JEKYLL_ENV=production bundle exec jekyll serve --host 0.0.0.0 --port 4000 --incremental --watch --force_polling
